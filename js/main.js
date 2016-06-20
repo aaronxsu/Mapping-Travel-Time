9639ef394552a99d438867b863f7d67b6b35ba9c
Xunze Su <xunzesu@outlook.com>
2016-06-20 14:39:34 -0400

    Initialization

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var city_Latlng = new google.maps.LatLng(40.7128, -74.0059);
+var mapOptions = {
+  zoom:11,
+  center: city_Latlng,
+  mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+var distanceMatrixService = new google.maps.DistanceMatrixService();
