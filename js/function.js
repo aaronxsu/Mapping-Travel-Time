2f927239ffee7ade9b4bfacb1b6a7a002456551c
Xunze Su <xunzesu@outlook.com>
2016-06-24 00:09:09 -0400

    refined search and filter

--- js/function.js
+++ js/function.js
@@ -9,38 +9,78 @@ var defaultStyle = function(colorToFill){
   };
 }
 
+var plotMaker = function (name, marker){
+  return marker.setIcon(L.divIcon({className: name})).addTo(map);
+}
+
+var plotGeoJsonLayer = function (data, color){
+  return L.geoJson(data, {style: defaultStyle(color)}).addTo(map);
+}
+
+var removeLeafletLayer = function (layer){
+  _.each(layer, function(datum){
+    map.removeLayer(datum);
+  });
+  layer = [];
+}
+
 $.ajax("https://raw.githubusercontent.com/aronxoxo/DanceNYC/master/data/dancenyc.geojson").done(function(data){
   danceNycParsed = JSON.parse(data);
   markerAllOrgs = _.chain(danceNycParsed.features)
-   .map(function(datum){
-    datum.properties.marker = L.marker([datum.geometry.coordinates[1], datum.geometry.coordinates[0]]);
-    return datum.properties.marker.setIcon(L.divIcon({className: 'icon-all'})).addTo(map);
-  }).value();
+                   .map(function(datum){
+                    datum.properties.marker = L.marker([datum.geometry.coordinates[1], datum.geometry.coordinates[0]]);
+                    return plotMaker('icon-all', datum.properties.marker)
+                  })
+                   .value();
   console.log(danceNycParsed);
 });
 
 $.ajax("https://raw.githubusercontent.com/aronxoxo/DanceNYC/master/data/NYC_ZCTA.geojson").done(function(data) {
   nycZcta = JSON.parse(data);
-  layerAllZcta = L.geoJson(nycZcta, {style: defaultStyle('#8b61ff')}).addTo(map);
+  layerAllZcta = plotGeoJsonLayer(nycZcta, '#8b61ff');
+  // console.log(nycZcta);
  });
 
  var filterAndPlot = function(){
    var orgLocation;
    var zipLocation = [];
    var zipNumber = [];
+   var boundPoints = [];
+
    if(inputOrgComplete){
+     $('#btn-clear_org_search').show();
      _.chain(danceNycParsed.features)
      .filter(function(datum){
        return datum.properties.ORG_NAME.toUpperCase() == inputOrgName.toUpperCase();
-     }).map(function(datum){
-       orgLocation = datum.geometry.coordinates;
+     })
+     .map(function(datum){
+       orgLocation = [datum.geometry.coordinates[1], datum.geometry.coordinates[0]];
        zipLocation.push([datum.properties.ZCTA_lat, datum.properties.ZCTA_lng]);
        zipNumber.push(datum.properties.ZCTA);
-     });
-     var concatedLocations = zipLocation.concat(orgLocation);
-     var locationBounds = L.LatLngBounds(concatedLocations);
-     //continue here
+     }).value();
+
+     map.setView(orgLocation, 15, {animate: true});
+     removeLeafletLayer(markerAllOrgs);
+     map.removeLayer(layerAllZcta);
+     inputOrgComplete = false;
+
+    var workforceZcta = _.map(zipNumber, function(zip){
+      return _.filter(nycZcta.features, function(datum){
+        return zip == datum.properties.zcta;
+      })
+    });
+
+    var workforceZctaFlatten = _.flatten(workforceZcta);
+
+    var workforceZctaFlattenToPlot = {
+      type: "FeatureCollection",
+      features: workforceZctaFlatten
+    };
+
+    // console.log(workforceZctaFlattenToPlot);
+    layerFilteredZcta = L.geoJson(workforceZctaFlattenToPlot, {style: defaultStyle("#4545ff")}).addTo(map);
    }
+   orgMarkerFocus = plotMaker('icon-focus', L.marker(orgLocation));
  };
 
  var onOrgSelection = function(){
@@ -49,10 +89,29 @@ $.ajax("https://raw.githubusercontent.com/aronxoxo/DanceNYC/master/data/NYC_ZCTA
    filterAndPlot();
  };
 
+ var onSearchClear = function(){
+   map.removeLayer(layerFilteredZcta);
+   layerFilteredZcta = [];
+   map.removeLayer(orgMarkerFocus);
+   orgMarkerFocus = [];
+   console.log(layerFilteredZcta, orgMarkerFocus);
+   map.setView([40.7831, -73.9712], 12, {animate: true});
+   layerAllZcta = plotGeoJsonLayer(nycZcta, '#8b61ff');
+   markerAllOrgs = _.chain(danceNycParsed.features)
+                    .map(function(datum){
+                     datum.properties.marker = L.marker([datum.geometry.coordinates[1], datum.geometry.coordinates[0]]);
+                     return plotMaker('icon-all', datum.properties.marker)
+                   });
+  $('#btn-clear_org_search').hide();
+  $('#org_name').val("");
+ };
+
  var bindEvents = function() {
    $('#org_name').keyup(onOrgSelection);
+   $('#btn-clear_org_search').click(onSearchClear);
  };
 
  $(document).ready(function() {
    bindEvents();
+   $('#btn-clear_org_search').hide();
  });
--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var map = L.map('map', {
   center: [40.7831, -73.9712],
-  zoom: 11
+  zoom: 12
 });
 var Stamen_Toner = new L.StamenTileLayer("toner");
 map.addLayer(Stamen_Toner);
@@ -9,5 +9,7 @@ var danceNycParsed;
 var markerAllOrgs;
 var nycZcta;
 var layerAllZcta;
-var inputOrgComplete = False;
+var inputOrgComplete = false;
 var inputOrgName;
+var layerFilteredZcta;
+var orgMarkerFocus;
