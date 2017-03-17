2f666b4ea2cdeca93b4956530e5453ebf5627aef
Xunze Su <xunzesu@afc108.wireless-pennnet.upenn.edu>
2017-03-17 12:29:24 -0400

    update basemap tile layer

--- js/main.js
+++ js/main.js
@@ -2,8 +2,13 @@ var map = L.map('map', {
   center: [40.741905, -73.911258],
   zoom: 12
 });
-var Stamen_Toner = new L.StamenTileLayer("toner");
-map.addLayer(Stamen_Toner);
+var cartoBasemap = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}@2x.png', {
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions">CARTO</a>',
+  subdomains: 'abcd',
+  minZoom: 0,
+  maxZoom: 20,
+  ext: 'png'
+}).addTo(map);
 
 var danceNycParsed;
 var isDanceNycParsed = false;
